a9881ddfddad454eb09178fded31dd2bf05cd28d
gisa chris <gisachris24@gmail.com>
2023-05-30 11:50:54 -0200

    add linter configurations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import _ from 'lodash';
-//import '../assets/images/logoDone.png';
 import './sass/styles.scss';
-
-const caller = async () => {
-  const baseUrl = 'https://api.tvmaze.com/'
-  const resource = 'https://api.tvmaze.com/shows/14';
-  const request = await fetch(resource)
-  const responce = await request.json();
-  console.log(responce);
-}
-
-caller();
